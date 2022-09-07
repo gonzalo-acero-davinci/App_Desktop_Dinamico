79e102727ca1cceddd2309d76f67d1ab3b7cb916
Sebastian <sebastian.peitiado@davinci.edu.ar>
2022-09-07 12:42:49 -0300

    GoogleAprobar Sebastian

--- Prueba_Git/tp-desktop/Program.cs
+++ Prueba_Git/tp-desktop/Program.cs
@@ -26,8 +26,26 @@ namespace tp_desktop
 
             string clientId = "793072394942-5iv7b3vh0s449e97d5kl5lb47ld5lvbe.apps.googleusercontent.com";
             string clientSecret = "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";
-
             
+            string[] scopes = {"https://www.googleapis.com/aupht/gmail.readonly"};
+            var credentials !UserCredential = GoogleWebauthorizationBroker.AuthorizeAsync(
+                new ClientSecrets
+                {
+                    clientId = clientId,
+                    clientSecret = clientSecret,
+                }, 
+                scopes, "User", CancellationToken.none).result;
+
+            if (credentials.Token.IsExpired(SystemClock.default))
+                credentials.RefreshTokenAsync(Cancellationtoken.None).Wait();
+
+            var service = new GmailService(new BaseClientService.Initializer()
+            {
+                httpClientInitializer = credentials
+            });
+
+            var profile = service.Users.GerProfile("gonzalo.Acero@davinci.edu.ar").Execute();
+            Console.ReadLine();
         }
     }
 }
